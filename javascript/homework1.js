cd040fba2be1a84f2cd2e72cc1927119844a1a46
abreezy <abeer.shahid1@gmail.comm>
2023-07-17 21:01:25 +0100

    created folders for organising and began JS content

---
+++ javascript/homework1.js
@@ -0,0 +1,12 @@
+var string1 = "Abeer"
+var string2 = " Shahid"
+console.log(string1 + string2)
+var num = 5
+var dayToday = "Monday"
+console.log(num)
+console.log(dayToday)
+var age = 28
+var year = 1994
+console.log("Hello, my name is" + " " + string1 + string2 + ", I am " + age + " and I was born in " + year)  
+var numString = 1+"beef"
+console.log(numString)
---
+++ javascript/lesson1.js
@@ -0,0 +1,9 @@
+// this is a single line comment
+/* This
+will 
+be a 
+multi-line 
+comment */
+console.log("hello you have executed the lesson 1 file")
+var name = "Abeer"
+console.log(name)
