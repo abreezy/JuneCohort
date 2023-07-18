7e2aa0fc261b7130bcaa6fe15f82c9844ac46747
abreezy <abeer.shahid1@gmail.comm>
2023-07-18 20:53:31 +0100

    lesson 2, intro to variable types and arrays

---
+++ javascript/lesson2.js
@@ -0,0 +1,43 @@
+/* let age = 25
+console.log(age)
+const name = "Abeer"
+
+age = `false`;
+console.log(age)
+
+// name = "Zain"
+console.log(typeof age)
+*/
+const name = "Abeer"
+let age = 25
+const dob = 98
+console.log(`my name is ${name} and I am years ${age} old and I was born in ${dob}`);
+
+
+//---------------
+
+// string
+// numbers
+// null
+// undefined
+// object
+// boolean
+
+// ["", 1, true, undefined, null, objects]
+
+let array = ["bread", "milk", "cheese", "fruits"]
+// position/index != number of items
+
+// array.length
+// array[n] = "something" will reassign item in that position
+// array.push("item")
+// array.pop()
+
+array[3] = "juice"
+array[4] = "pasta"
+array.push(name)
+array.splice(2, 1)
+
+delete array[1]
+
+console.log(array)
