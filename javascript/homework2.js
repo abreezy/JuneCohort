cbf139198a9b5494d64fabc1dc636c77aba11d96
abreezy <abeer.shahid1@gmail.comm>
2023-07-26 17:27:03 +0100

    updating lesson 2 JS homework

--- javascript/homework2.js
+++ javascript/homework2.js
@@ -10,7 +10,7 @@ console.log(list)
 // Find the position of the first item, middle item and last item of the list and print them off separately.
 console.log(list[0])
 console.log(list[6])
-console.log(list[14])
+console.log(list[list.length-1])
 // Modify 3 items in the array and print them off
 list[2] = 'three'
 list[3] = 'four'
@@ -21,12 +21,16 @@ console.log(list[4])
 // Using string interpolation combine 5 different variables together
 console.log(`${list[0]} is before ${list[1]}, ${list[2]} is before ${list[3]}. After is ${list[4]}`)
 // Delete the 2 items in the array
-delete list[3]
-delete list[1]
+// delete list[3]
+// delete list[1]
+// console.log(list)
+// delete better used for objects not arrays
+list.shift()
 console.log(list)
 // remove the first item from the array
 list.splice(0, 1)
 console.log(list)
 // add an item to the first position of the array
 list.splice(0, 0, 'apple')
+list.unshift("biscuit")
 console.log(list)
