f6078d9ced57134e5d9142d55839fb2438576463
abreezy <abeer.shahid1@gmail.comm>
2023-08-04 22:53:40 +0100

    completed Challenge 2 of homework5 JS

--- javascript/homework5.js
+++ javascript/homework5.js
@@ -3,7 +3,7 @@
 Using a loop, print numbers from 1 to 100 but here’s the catch, multiple of 3 should print “Fizz” and multiples of 5 should print “Buzz”. Lastly print “FizzBuzz” for multiples of three and five.
 */
 
-for (let i = 1; i <= 100; i++) {
+/* for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
     console.log("Fizzbuzz")
 } else if (i % 5 === 0) {
@@ -14,3 +14,25 @@ for (let i = 1; i <= 100; i++) {
     console.log(i)
 }
 }
+
+/* ## Challenge 2:
+
+Take the following phrase and translate it to a different word depending on vowels shown.
+phrase “turpentine and turtles” 
+word equivalent "UUEEIEEAUUEE”
+Note for every u&e character in the phrase the word equivalent should contain 2 of those characters. 
+If it is not a vowel it is not stored in the word equivalent. 
+*/
+
+var phrase = "turpentine and turtles" 
+var equivalent = ""
+for (let i = 0; i < phrase.length; i++) {
+    if (phrase[i] === "a" || phrase[i] === "e" || phrase[i] === "i" || phrase[i] === "o" || phrase[i] === "u") {
+        equivalent = equivalent.concat(phrase[i])
+    }
+    if (phrase[i] === "e" || phrase[i] === "u"){
+        equivalent = equivalent.concat(phrase[i])
+    }
+}
+console.log(equivalent.toUpperCase())
+    
