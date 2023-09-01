4289eac8661754489774ad6737143326655dbbff
abreezy <abeer.shahid1@gmail.comm>
2023-09-02 00:26:15 +0100

    completing event listeners in forms homework, email regex

--- javascript/doms/lessons/forms.js
+++ javascript/doms/lessons/forms.js
@@ -9,16 +9,31 @@
 
 const body = document.body
 const head = document.head
-const nameInput = document.getElementById("Name")
+const nameInput = document.getElementById("name")
+const lastNameInput = document.getElementById("lastName")
+const email = document.getElementById("email")
 const form = document.querySelector("form")
+
+//const emailPattern = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 
 function checkName() {
     var result = false
+    var result2 = false
+    var resultEmail = false
     if (nameInput.value.length >= 2 && nameInput.value.length <= 20){
         result = stringRegex(nameInput.value)
         console.log(result)
     }
-    return result
+    if (lastNameInput.value.length >= 2 && lastNameInput.value.length <= 20){
+        result2 = stringRegex(lastNameInput.value)
+        console.log(result2)
+    }
+    if (email.value.match(emailPattern)) {
+        resultEmail = emailRegex(email.value)
+        console.log(resultEmail)
+    }
+    return {result, result2, resultEmail}
 }
 
 function stringRegex(param){
@@ -26,12 +41,26 @@ function stringRegex(param){
     return pattern.test(param)
 }
 
-form.addEventListener("submit", function(e){
-    e.preventDefault()
-    var outcome = checkName()
-    if (outcome === true){
-        alert("form passes")
+function emailRegex(param2) {
+    return emailPattern.test(param2)
+}
+
+// form.addEventListener("submit", function(e){
+//     e.preventDefault()
+//     var outcome = checkName()
+//     if (outcome === true){
+//         alert("form passes")
+//     } else {
+//         alert("form fails")
+//     }
+// })
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var { result, result2, resultEmail } = checkName();
+    if (result && result2 && resultEmail) {
+        alert("form passes");
     } else {
-        alert("form fails")
+        alert("form fails");
     }
 })
