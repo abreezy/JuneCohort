e71b82adc1846d2678f1804582fd87219d06b381
abreezy <abeer.shahid1@gmail.comm>
2023-09-18 21:32:33 +0100

    homework for using data set to pull props from and display using the map function

--- javascript/react/react-homework/src/App.js
+++ javascript/react/react-homework/src/App.js
@@ -1,78 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import Nav from './Nav';
-import Footer from './Footer';
-import Card from './Card';
+import React from 'react'
+import './App.css'
+import Nav from './Nav'
+import MoviesCard from './Card'
+import data from './data.json'
 
 function App() {
+
+  var number = 0
+  const movieslist = data
+  .filter((item) => item.year > 2020)
+  .map((item) => {
+    return (
+      <MoviesCard
+      key = {number++}
+      thumbnail = {item.thumbnail}
+      title = {item.title}
+      year = {item.year}
+      genres = {item.genres.join(", ")}
+      extract = {item.extract}
+      />
+    )
+  }) 
+
+
   return (
     <div>
-            <header>
-                <Nav />
-            </header>
-            <main className='card-grid'>
-                <Card
-                img = "https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/Harry_Potter_and_the_Philosopher%27s_Stone_Book_Cover.jpg/220px-Harry_Potter_and_the_Philosopher%27s_Stone_Book_Cover.jpg"
-                title = "Harry Potter and the Philosopher’s Stone"
-                author = "J.K. Rowling"
-                release = "1997"
-                price = "£10" 
-                alt = "Harry Potter and the Philosopher’s Stone"
-                />
-                <Card
-                img = "https://upload.wikimedia.org/wikipedia/en/5/5c/Harry_Potter_and_the_Chamber_of_Secrets.jpg"
-                title = "Harry Potter and the Chamber of Secrets"
-                author = "J.K. Rowling"
-                release = "1998"
-                price = "£10" 
-                alt = "Harry Potter and the Chamber of Secrets"
-                />
-                <Card
-                img = "https://upload.wikimedia.org/wikipedia/en/thumb/a/a0/Harry_Potter_and_the_Prisoner_of_Azkaban.jpg/220px-Harry_Potter_and_the_Prisoner_of_Azkaban.jpg"
-                title = "Harry Potter and the Prisoner of Azkaban"
-                author = "J.K. Rowling"
-                release = "1999"
-                price = "£10" 
-                alt = "Harry Potter and the Prisoner of Azkaban"
-                />
-                <Card
-                img = "https://upload.wikimedia.org/wikipedia/en/b/b6/Harry_Potter_and_the_Goblet_of_Fire_cover.png"
-                title = "Harry Potter and the Goblet of Fire"
-                author = "J.K. Rowling"
-                release = "2000"
-                price = "£10" 
-                alt = "Harry Potter and the Goblet of Fire"
-                />
-                <Card
-                img = "https://upload.wikimedia.org/wikipedia/en/thumb/7/70/Harry_Potter_and_the_Order_of_the_Phoenix.jpg/220px-Harry_Potter_and_the_Order_of_the_Phoenix.jpg"
-                title = "Harry Potter and the Order of the Phoenix"
-                author = "J.K. Rowling"
-                release = "2003"
-                price = "£10" 
-                alt = "Harry Potter and the Order of the Phoenix"
-                />
-                <Card
-                img = "https://upload.wikimedia.org/wikipedia/en/thumb/b/b5/Harry_Potter_and_the_Half-Blood_Prince_cover.png/220px-Harry_Potter_and_the_Half-Blood_Prince_cover.png"
-                title = "Harry Potter and the Half-Blood Prince"
-                author = "J.K. Rowling"
-                release = "2005"
-                price = "£10" 
-                alt = "Harry Potter and the Half-Blood Prince"
-                />
-                <Card
-                img = "https://upload.wikimedia.org/wikipedia/en/thumb/a/a9/Harry_Potter_and_the_Deathly_Hallows.jpg/220px-Harry_Potter_and_the_Deathly_Hallows.jpg"
-                title = "Harry Potter and the Deathly Hallows"
-                author = "J.K. Rowling"
-                release = "2007"
-                price = "£10" 
-                alt = "Harry Potter and the Deathly Hallows"
-                />
-            </main>
-            <footer>
-                < Footer />
-            </footer>
-        </div>
-  );
+      <header>
+        <h2>Movies.io</h2>
+        <Nav />
+      </header>
+      <main className='card-grid'>
+        {movieslist}
+      </main>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- javascript/react/react-homework/src/Card.js
+++ javascript/react/react-homework/src/Card.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
-function Card(props) {
+function MoviesCard(props) {
+    return (
+        <div classname="cardComponent">
+          {props.thumbnail ? <img src={props.thumbnail} alt={props.alt}></img> : <img src="https://spng.pngfind.com/pngs/s/2-24050_question-mark-png-image-transparent-white-question-mark.png" alt="not displayed"></img>}
+          {props.title ? <h4>{props.title}</h4> : <h4>Not specified</h4>}
+          {props.genres ? <p>{props.genres}</p> : <p>Not listed</p>}
+          {props.extract ? <p>{props.extract}</p> : <p>Details to be released soon</p>}
+        </div>
+      )
+    }
 
-  return (
-    <div classname="cardComponent">
-      {props.img ? <img src={props.img}></img> : <img src="https://spng.pngfind.com/pngs/s/2-24050_question-mark-png-image-transparent-white-question-mark.png" alt="not displayed"></img>}
-      {props.title ? <h4>{props.title}</h4> : <h4>Unkown title</h4>}
-      {props.author ? <p>{props.author}</p> : <p>Unkown author</p>}
-      {props.release ? <p>{props.release}</p> : <p>Unkown release date</p>}
-      {props.price ? <h3>{props.price}</h3> : <h3>Ask for quote</h3>}
-    </div>
-  )
-}
-
-export default Card
+export default MoviesCard
